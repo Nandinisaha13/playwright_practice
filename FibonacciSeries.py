531351878de5e700f042502a9e07f7f1ad0e0dbc
nandinisaha13 <nandinisaha13@gmail.com>
2026-02-16 13:14:36 +0530

    fibonacci series

---
+++ FibonacciSeries.py
@@ -0,0 +1,16 @@
+count=19
+a= 0
+b= 1
+
+print(f"Start: {a}, {b}", end=',')
+
+for _ in range(2,count):
+    next_sum=a+b
+
+    print(next_sum, end=",")
+
+    a=b
+    b=next_sum
+print("done")
+
+
