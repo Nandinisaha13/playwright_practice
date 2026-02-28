618807b99a4496b073c1a61f89ecce01dada3725
nandinisaha13 <nandinisaha13@gmail.com>
2026-02-28 19:29:31 +0530

    prime number

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -1,10 +1,10 @@
-num =7
+num =190029
 if num<=1:
     print("Not a prime number")
 else:
     is_prime= True
 
-for i in range(2,num):
+for i in range(2, int(num ** 0.5) + 1):
     if num %i ==0:
         print(f"failed {num} is divisible by {i}")
         is_prime= False
